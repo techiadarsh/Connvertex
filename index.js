a5b77a5f9ac9b89d12d850b0c7bae932a5086eee
techiadarsh <singhadarsh0205@gmail.com>
2018-08-24 14:20:17 +0530

    changiing port and optimizing the code

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/form.html');
 });
 
-app.get('/submit',(req,res) => {
+app.get('/submit', (req, res) => {
   weather
   .fetch(req.query.A,req.query.X,req.query.C)
   .then(response=>{res.write(response);res.end();})
   .catch(error=>{console.log(error);res.end();});
 });
   
-app.listen(8080);
+app.listen(8000);
